04fb4e98d37121dbf5f57bde9c0606a2fd07f48d
boonruang <scubatoy@gmail.com>
2023-02-19 09:01:09 +0700

    setup user form

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Routes,Route} from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashbaord from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -27,10 +27,10 @@ function App() {
               <Topbar />
               <Routes>
                   <Route path="/" element={<Dashbaord />} />
-                  {/* <Route path="/team" element={<Team />} /> */}
-                  {/* <Route path="/contacts" element={<Contacts />} /> */}
-                  {/* <Route path="/invoices" element={<Invoices />} /> */}
-                  {/* <Route path="/form" element={<Form />} /> */}
+                  <Route path="/team" element={<Team />} />
+                  <Route path="/contacts" element={<Contacts />} />
+                  <Route path="/invoices" element={<Invoices />} />
+                  <Route path="/form" element={<Form />} />
                   {/* <Route path="/bar" element={<Bar />} /> */}
                   {/* <Route path="/line" element={<Line />} /> */}
                   {/* <Route path="/pie" element={<Pie />} /> */}
