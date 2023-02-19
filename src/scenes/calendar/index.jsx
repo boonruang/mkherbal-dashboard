01e215bafb5451882a176f0dbd167dcb0f58ade4
boonruang <scubatoy@gmail.com>
2023-02-19 12:44:19 +0700

    done calendar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -23,6 +23,8 @@ const Calendar = () => {
     const [currentEvents, setCurrentEvents] = useState([])
 
     const handleDateClick = (selected) => {
+
+        // console.log('select', selected)
         const title = prompt("Please enter a new title for your event")
         const calendarApi = selected.view.calendar
         calendarApi.unselect()
@@ -39,6 +41,7 @@ const Calendar = () => {
     }
 
     const handleEventClick = (selected) => {
+        // console.log('event click', selected)
         if (window.confirm(`Are you sure you want to delete the event '${selected.event.title}' `)) {
             selected.event.remove()
         }
@@ -77,7 +80,36 @@ const Calendar = () => {
                         </ListItem>
                     ))}
                 </List>
+            </Box>
 
+            {/* CALENDAR */}
+            <Box flex="1 1 100%" ml="15px">
+                <Fullcalendar
+                    height="75vh"
+                    plugins={[
+                        dayGridPlugin,
+                        timeGridPlugin,
+                        interactionPlugin,
+                        listPlugin
+                    ]}
+                    headerToolbar={{
+                        left: "prev,next,today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                    }}
+                    initialView="dayGridMonth"
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
+                    dayMaxEvents={true}
+                    select={handleDateClick}
+                    eventClick={handleEventClick}
+                    eventsSet={(events) => setCurrentEvents(events)}
+                    initialEvents={[
+                        { id: "1234", title: "All-day event", date: "2023-02-19" },
+                        { id: "2535", title: "Timed event", date: "2023-02-28" },
+                    ]}
+                />
             </Box>
         </Box>
     </Box>
