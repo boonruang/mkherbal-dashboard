edcd25c56f05493528f1232ace412a756451e390
Boonruang Seedapunt <scubatoy@gmail.com>
2024-12-07 23:46:56 +0700

    set timeout and navigate to login page

--- src/components/Thankyoupage.jsx
+++ src/components/Thankyoupage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { 
-    Box, 
-  } from '@mui/material'
+import { useNavigate } from "react-router-dom";
+import { Box } from '@mui/material'
   
 const ThankyouReg = () => {
+  const navigate = useNavigate()
+
+  setTimeout(() => {
+    navigate('/login')
+  },2000)
+
 
     return <Box m="20px">
             Thank you.
